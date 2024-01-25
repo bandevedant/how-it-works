3b0e34f8364ac66ffffb9b3055a25cb60bac7836
Vedant Bande <vedantbande2002@gmail.com>
2024-01-25 15:52:24 +0530

    refactoring changes

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -9,11 +9,12 @@ const boxStyle = {
   display: "flex",
   flexDirection: "column", // Set to column
   alignItems: "center",
+  justifyContent:"center",
   border: "grey solid 2px",
   borderRadius: "50%",
   padding: "5px",
-  width: "175px",
-  height: "175px",
+  width: "180px",
+  height: "180px",
   color: "#fff",
   marginRight: "80px",
   textAlign: "center", // Center text
@@ -33,7 +34,7 @@ const DraggableBox = ({ id, desc, url }) => {
         </div>
         <span className="steps-desc">
           <h4>{id}</h4>
-          <h5>{desc}</h5>
+          <p>{desc}</p>
         </span>
       </div>
     </Draggable>
@@ -49,28 +50,32 @@ export default function Arrow() {
   ];
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Xwrapper>
-        {steps.map((step, index) => (
-          <DraggableBox
-            key={step.id}
-            id={step.id}
-            desc={step.desc}
-            url={step.url}
-            style={{
-              marginRight: index !== steps.length - 1 ? "80px" : "0",
-            }}
-          />
-        ))}
-        <Xarrow start={"Step 1:"} end="Step 2:" dashness />
-        <Xarrow start={"Step 2:"} end="Step 3:" dashness />
-        <Xarrow start={"Step 3:"} end="Step 4:" dashness />
-      </Xwrapper>
-    </div>
+    <center>
+      <h2 style={{ marginBottom: "80px", fontSize: "50px" }}>How it works?</h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          
+        }}
+      >
+        <Xwrapper>
+          {steps.map((step, index) => (
+            <DraggableBox
+              key={step.id}
+              id={step.id}
+              desc={step.desc}
+              url={step.url}
+              style={{
+                marginRight: index !== steps.length - 1 ? "80px" : "0",
+              }}
+            />
+          ))}
+          <Xarrow start={"Step 1:"} end="Step 2:" dashness />
+          <Xarrow start={"Step 2:"} end="Step 3:" dashness />
+          <Xarrow start={"Step 3:"} end="Step 4:" dashness />
+        </Xwrapper>
+      </div>
+    </center>
   );
 }
